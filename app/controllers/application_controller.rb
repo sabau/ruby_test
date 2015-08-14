9f02420d91bc37234a996f1869dfbc8cc96d8734
Karoly Szabo <karoly.szabo@intesys.it>
2015-08-14 09:38:00 +0200

    fix sex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password, :is_female, :date_of_birth) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password, :is_male, :date_of_birth) }
     end
 end
